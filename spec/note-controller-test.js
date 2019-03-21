3ea36981ed3fa3238acdc3e21b1ac296527c6d18
Oliver <ofcharrison@gmail.com>
2019-03-21 17:44:30 +0000

    got it to work Luke

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -1,13 +1,13 @@
 (function(exports) {
   function testControllerCanBeInstatiated() {
     console.log("You should see me");
-    let control = new NoteController();
+    var control = new NoteController();
     console.log("Me too");
     if (control instanceof NoteController){
-      console.log(`${controller} is an instance of your Note Controller`)
+      console.log(`${control} is an instance of your Note Controller`)
     }
     else {
-      console.log(`${controller} is NOT an instance. Oh no.`)
+      console.log(`${control} is NOT an instance. Oh no.`)
       throw new Error("Error: not an instance of NoteController");
     }
   };
